7f70807d2415490239cfa5ce255d64ffa250f3b1
Duncan McGreggor <dmcgreggor@usgs.gov>
2015-11-24 12:36:55 -0600

    More target tweaks.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -1,13 +1,13 @@
 .PHONY: docs
 REPO = $(shell git config --get remote.origin.url)
-CWD = $(shell pwd)
-DOCS_DIR = $(CWD)/docs
+ROOT_DIR = $(shell pwd)
+DOCS_DIR = $(ROOT_DIR)/docs
 DOCS_BUILD_DIR = $(DOCS_DIR)/build
 DOCS_PROD_DIR = $(DOCS_DIR)/master
 SLATE_GIT_HACK = $(DOCS_DIR)/.git
 
 $(SLATE_GIT_HACK):
-	cd $(DOCS_DIR) && ln -s ../.git .
+	ln -s $(ROOT_DIR)/.git $(DOCS_DIR)
 
 docs-setup:
 	cd docs && bundle install
@@ -22,11 +22,12 @@ commit:
 	-git commit -a && git push --all
 
 publish: commit docs
-	rm -rf $(DOCS_PROD_DIR)/current $(DOCS_DIR)/.git $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
+	rm -rf $(DOCS_PROD_DIR)/current $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
 	cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
 	cd $(DOCS_PROD_DIR) && \
 	git init && \
 	git add * &> /dev/null && \
 	git commit -a -m "Generated content." &> /dev/null && \
 	git push -f $(REPO) master:gh-pages
-	rm -rf $(DOCS_DIR)/.git $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
+	rm $(DOCS_DIR)/.git
+	rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
