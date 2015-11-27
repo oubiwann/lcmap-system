c0f8427b284aae97fc44c2fb0b3ca0ee9bee6d53
Duncan McGreggor <oubiwann@gmail.com>
2015-11-27 09:51:22 -0600

    Formatting (new lines for every message).

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -11,27 +11,27 @@ $(SLATE_GIT_HACK):
 	ln -s $(ROOT_DIR)/.git $(DOCS_DIR)
 
 docs-setup:
-	@echo "Installing and setting up dependencies ..."
+	@echo "\nInstalling and setting up dependencies ..."
 	@cd $(DOCS_DIR) && bundle install
 
 devdocs:
-	@echo "Running development server ..."
+	@echo "\nRunning development server ..."
 	@cd $(DOCS_DIR) && bundle exec middleman server
 
 clean-build:
-	@echo "Cleaning build directory ..."
+	@echo "\nCleaning build directory ..."
 	@rm -rf $(DOCS_BUILD_DIR)
 
 docs: clean-build
-	@echo "Building docs ..."
+	@echo "\nBuilding docs ..."
 	@cd $(DOCS_DIR) && bundle exec middleman build --clean
 
 commit:
-	@echo "Commiting changes ..."
+	@echo "\nCommiting changes ..."
 	@-git commit -a && git push --all
 
 setup-temp-repo: $(SLATE_GIT_HACK)
-	@echo "Setting up temporary git repos for gh-pages ..."
+	@echo "\nSetting up temporary git repos for gh-pages ..."
 	@rm -rf $(DOCS_PROD_DIR)/current $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
 	@cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
 	@cd $(DOCS_PROD_DIR) && git init
@@ -39,11 +39,11 @@ setup-temp-repo: $(SLATE_GIT_HACK)
 	@cd $(DOCS_PROD_DIR) && git commit -a -m "Generated content." > /dev/null
 
 teardown-temp-repo:
-	@echo "Tearing down temporary gh-pages repos ..."
+	@echo "\nTearing down temporary gh-pages repos ..."
 	@rm $(DOCS_DIR)/.git $(DOCS_DIR)/Gemfile.lock
 	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
 
 publish: commit docs setup-temp-repo
-	@echo "Publishing docs ..."
+	@echo "\nPublishing docs ..."
 	@cd $(DOCS_PROD_DIR) && git push -f $(REPO) master:gh-pages
 	@make teardown-temp-repo
