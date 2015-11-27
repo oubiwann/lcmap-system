29b619a4f814a5c33d4343372f5293cead8ad188
Duncan McGreggor <oubiwann@gmail.com>
2015-11-27 09:50:20 -0600

    Cleanup up make targets.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -11,31 +11,39 @@ $(SLATE_GIT_HACK):
 	ln -s $(ROOT_DIR)/.git $(DOCS_DIR)
 
 docs-setup:
-	cd $(DOCS_DIR) && bundle install
+	@echo "Installing and setting up dependencies ..."
+	@cd $(DOCS_DIR) && bundle install
 
 devdocs:
-	cd $(DOCS_DIR) && bundle exec middleman server
+	@echo "Running development server ..."
+	@cd $(DOCS_DIR) && bundle exec middleman server
 
 clean-build:
-	rm -rf $(DOCS_BUILD_DIR)
+	@echo "Cleaning build directory ..."
+	@rm -rf $(DOCS_BUILD_DIR)
 
 docs: clean-build
-	cd $(DOCS_DIR) && bundle exec middleman build --clean
+	@echo "Building docs ..."
+	@cd $(DOCS_DIR) && bundle exec middleman build --clean
 
 commit:
-	-git commit -a && git push --all
+	@echo "Commiting changes ..."
+	@-git commit -a && git push --all
 
 setup-temp-repo: $(SLATE_GIT_HACK)
-	rm -rf $(DOCS_PROD_DIR)/current $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
-	cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
-	cd $(DOCS_PROD_DIR) && git init
-	cd $(DOCS_PROD_DIR) && git add * > /dev/null
-	cd $(DOCS_PROD_DIR) && git commit -a -m "Generated content." > /dev/null
+	@echo "Setting up temporary git repos for gh-pages ..."
+	@rm -rf $(DOCS_PROD_DIR)/current $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
+	@cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
+	@cd $(DOCS_PROD_DIR) && git init
+	@cd $(DOCS_PROD_DIR) && git add * > /dev/null
+	@cd $(DOCS_PROD_DIR) && git commit -a -m "Generated content." > /dev/null
 
 teardown-temp-repo:
-	rm $(DOCS_DIR)/.git $(DOCS_DIR)/Gemfile.lock
-	rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
+	@echo "Tearing down temporary gh-pages repos ..."
+	@rm $(DOCS_DIR)/.git $(DOCS_DIR)/Gemfile.lock
+	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
 
 publish: commit docs setup-temp-repo
-	cd $(DOCS_PROD_DIR) && git push -f $(REPO) master:gh-pages
-	make teardown-temp-repo
+	@echo "Publishing docs ..."
+	@cd $(DOCS_PROD_DIR) && git push -f $(REPO) master:gh-pages
+	@make teardown-temp-repo
